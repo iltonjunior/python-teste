30e0de227498a4663ff7c0c947988281968d77e1
Ilton Vieira de Arruda Júnior <34026753+iltonjunior@users.noreply.github.com>
2022-05-19 18:57:20 -0300

    Update web.py

--- web.py
+++ web.py
@@ -1,11 +1,11 @@
 from flask import Flask
+application = Flask(__name__)
 
-app = Flask(__name__)
+@application.route('/hello')
+def hello():
+    return "Hello World from Python Flask!"
 
-@app.route('/hello')
-def helloIndex():
-    return 'Hello World from Python Flask!'
-
-#app.run(host='0.0.0.0', port=5000)
 if __name__ == "__main__":
     application.run()
+
+#app.run(host='0.0.0.0', port=5000)
